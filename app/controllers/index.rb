7f5d99ce0f8f8241b9a59e5d1bc038a6645ada62
Promoveo <zappeln@gmail.com>
2013-11-12 14:41:27 -0600

    enable to post tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,34 +6,31 @@ end
 
 
 get '/user' do
-  redirect to '/' + params[:username]
+  redirect to '/user/' + params[:username]
 end
 
 
-get '/:username' do
-
+get '/user/:username' do
   @user = TwitterUser.find_by_username(params[:username])
   if @user
     if @user.tweets.empty?
-      # User#fetch_tweets! should make an API call
-      # and populate the tweets table
-      #
-      # Future requests should read from the tweets table
-      # instead of making an API call
       @user.fetch_tweets!
     end
   else
     @user = TwitterUser.create(username: params[:username])
     if @user.tweets.empty?
-      # User#fetch_tweets! should make an API call
-      # and populate the tweets table
-      #
-      # Future requests should read from the tweets table
-      # instead of making an API call
       @user.fetch_tweets!
     end
   end
-  # @user.tweets.each {|tweet| tweet.body }
   @tweets = @user.tweets.limit(10)
   erb :user
+end
+
+get '/tweet' do
+  erb :create_tweet
+end
+
+post '/tweet' do
+  Twitter.update(params[:body])
+  erb :index
 end
